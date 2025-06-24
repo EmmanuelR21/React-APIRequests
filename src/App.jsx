a0c85b399d205332bc64377eee7d63aff842f7b7
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2025-06-24 14:15:09 -0400

    Add search functionality to GIPHY API integration

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const GIPHY_API_KEY = "ktJr7BVC2NkuHL6VwDNNFoBwRRZeWgQl";
 
 const App = () => {
   const [gifs, setGifs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   async function fetchData() {
     const data = await axios.get(
@@ -16,20 +17,44 @@ const App = () => {
     setGifs(gifData);
     console.log(gifData);
   }
+  async function fetchBasedOnSearch() {
+    const data = await axios.get(
+      `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${GIPHY_API_KEY}`,
+    );
+    const gifData = data.data.data;
+    setGifs(gifData);
+    console.log(gifData);
+  }
+  function handleClick(event) {
+    event.preventDefault();
+    setSearchTerm(event.target[0].value);
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (searchTerm === "") {
+      fetchData();
+    } else {
+      fetchBasedOnSearch();
+    }
+  }, [searchTerm]);
+
   return (
     <div className="app">
       <h1 className="title">Let's Make Some API Requests!</h1>
-      {gifs.map((gifObject) => (
-        <img key={gifObject.url} src={gifObject.images.original.url} />
-      ))}
+      <form onSubmit={handleClick}>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        {gifs.map((gifObject) => (
+          <div className="gif-card">
+            <img key={gifObject.url} src={gifObject.images.original.url} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 
 // The following lines initialize your React application and inject
 // it into the index.html
